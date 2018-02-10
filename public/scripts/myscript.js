9560ae7711e37ca5b749330f00d20a2bdc54484c
David Marshall <epper.marshall@gmail.com>
2018-02-10 15:02:08 -0500

    datepicker update for adding trip

--- public/scripts/myscript.js
+++ public/scripts/myscript.js
@@ -113,11 +113,15 @@ $(function(){
     var hours=["12:00","12:30","1:00","1:30","2:00","2:30","3:00","3:30","4:00","4:30","5:00","5:30","6:00","6:30","7:00","7:30","8:00","8:30","9:00","9:30","10:00","10:30","11:00","11:30"];
     $('#departDate').datepicker({minDate:0,onSelect:function(date){
         var selDate=new Date(date);
-        var stDate=new Date(selDate.getTime()-86400000);
-        $('#returnDate').datepicker('option','minDate',stDate)
+        //var stDate=new Date(selDate.getTime()-86400000);
+        $('#returnDate').datepicker('option','minDate',selDate)
     }}).datepicker('setDate',new Date());
     $('#returnDate').datepicker({minDate:0}).datepicker('setDate',new Date());
-    $('#dDate').datepicker({minDate:0}).datepicker('setDate',new Date());
+    $('#dDate').datepicker({minDate:0,onSelect:function(date){
+        var selDate=new Date(date);
+        //var stDate=new Date(selDate.getTime()-86400000);
+        $('#rDate').datepicker('option','minDate',selDate)
+    }}).datepicker('setDate',new Date());
     $('#rDate').datepicker({minDate:0}).datepicker('setDate',new Date());
     for(var i=0;i<2;i++){
         var t=i==0?' am':' pm';
