614d071b60a96c16280a40156e9c012f421075ac
depperm <epper.marshall@gmail.com>
2018-02-14 19:34:56 -0500

    array index

--- app.js
+++ app.js
@@ -224,10 +224,10 @@ app.get('/api/passengers/:trip',function(req,res){
     db.collection('Trips').find({'_id':ObjectId(req.params.trip)}).toArray(function(error, trip) {
         if (error) throw error;
         console.log(JSON.stringify(trip))
-        console.log('passengers' in trip)
-        console.log(trip['passengers'])
-        if('passengers' in trip){
-            db.collection('Users').find({'userId':{$in:trip['passengers']}}).toArray(function(err,details){
+        console.log('passengers' in trip[0])
+        console.log(trip[0]['passengers'])
+        if('passengers' in trip[0]){
+            db.collection('Users').find({'userId':{$in:trip[0]['passengers']}}).toArray(function(err,details){
                 if(err) throw err;
                 console.log('found details:'+JSON.stringify(details))
                 res.send(details)
