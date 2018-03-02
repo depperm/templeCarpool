eab4ffa9fe0fd565fc16c2c7062558e1dee3b143
depperm <epper.marshall@gmail.com>
2018-03-01 21:12:36 -0500

    fix backend?

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ app.post('/api/users/emailPrefs/:email',function(req,res){
         //console.log(results)
         res.send(results)
     })*/
-    db.collection('Users').update({'email':req.params.email},{$set:(req.body)},function(err,result){
+    db.collection('Users').update({'email':req.params.email},{$set:(req.body)},{upsert: true, safe: false},function(err,result){
         if(err){
             res.status(500).send('Some error:'+err)
             return
