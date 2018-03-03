e6ff987a56f2eb1ad8b01a8b9fff8bc1f0cedbd1
depperm <epper.marshall@gmail.com>
2018-03-03 11:25:49 -0500

    promise resovle

--- app.js
+++ app.js
@@ -336,10 +336,9 @@ app.post('/api/trips/edit/:trip',function(req,res){
             console.log('the trip:'+JSON.stringify(trip))
             if('passengers' in trip[0] && trip[0].passengers.length>0){
                 //for(var i=0;i<trip[0].passengers.length;i++){
-                var i=0;
-                while(i<trip[0].passengers.length){
+                for(var i=0,p=checkEmailPref(trip[0].passengers[i].email,'tripModified').resolve();i<trip[0].passengers.length;i++)
                     console.log('checking: '+JSON.stringify(trip[0].passengers[i]))
-                    checkEmailPref(trip[0].passengers[i].email,'tripModified').then(function(shouldEmail){
+                    p=p.then(function(shouldEmail){
                         if(shouldEmail){
                             console.log('should send to '+JSON.stringify(trip[0].passengers[i]))
                             sendEmail(trip[0].passengers[i].email,'The driver has modified your trip from '+req.body.dDate+'-'+req.body.rDate+'. Please visit templecarpool.com to change your email preferences or to see the Trip changes.')
